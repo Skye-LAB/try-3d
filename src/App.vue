bfe656a03bca953a9c967aeccae4767f4ed88e28
Skye-LAB <walksky452@gmail.com>
2022-01-22 17:23:40 +0000

    add: card

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,44 @@
 <template>
-  <Renderer ref="renderer">
-    <Camera :position="{ z: 10 }" />
-    <Scene>
-      <PointLight :position="{ y: 50, z: 50 }" />
-      <Box ref="box">
-        <LambertMaterial />
-      </Box>
-    </Scene>
-  </Renderer>
+  <Card>
+    <template v-slot:card-img>
+      <Renderer ref="renderer">
+        <Camera :position="{ z: 10 }" />
+        <Scene>
+          <PointLight :position="{ y: 50, z: 50 }" />
+          <GltfModel src="./scene.gltf"/>
+        </Scene>
+      </Renderer>
+    </template>
+    <template v-slot:card-title> Product </template>
+    <template v-slot:card-body>
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </template>
+  </Card>
 </template>
 <script>
 import { reactive, ref } from "@vue/reactivity";
 import { onMounted } from "@vue/runtime-core";
+
+import Card from "./components/Card.vue";
 export default {
+  components: {
+    Card,
+  },
   setup() {
     const renderer = ref();
-    const box = ref();
 
     const _ = reactive({
       renderer,
-      box,
     });
 
     onMounted(() => {
       _.renderer.onBeforeRender(() => {
-        _.box.mesh.rotation.x += 0.1;
       });
     });
 
     return {
       renderer,
-      box,
     };
   },
 };
---
+++ src/components/Card.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="card" style="width: 18rem">
+    <slot name="card-img" />
+    <div class="card-body">
+      <h5 class="card-title">
+        <slot name="card-title" />
+      </h5>
+      <p class="card-text">
+        <slot name="card-body" />
+      </p>
+      <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
